6913c6523d6083765be6171d183f5a55f0f5cdde
oliversalmon <oliver.salmon@gmail.com>
2016-10-21 06:32:41 +0100

    Removed intellij files and directories

--- datagrid/src/main/java/com/example/projects/dimensionLoaders/InstrumentLoader.java
+++ datagrid/src/main/java/com/example/projects/dimensionLoaders/InstrumentLoader.java
@@ -4,4 +4,8 @@ package com.example.projects.dimensionLoaders;
  * Created by oliverbuckley-salmon on 16/10/2016.
  */
 public class InstrumentLoader {
+
+    public static void main(String[] argv) {
+
+    }
 }
--- datagrid/src/main/java/com/example/projects/dimensionLoaders/VenueLoader.java
+++ datagrid/src/main/java/com/example/projects/dimensionLoaders/VenueLoader.java
@@ -1,7 +1,27 @@
 package com.example.projects.dimensionLoaders;
 
+import com.example.projects.domain.Venue;
+import com.example.projects.domain.enums.VenueTypeEnum;
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
+
+
+import java.util.Map;
+import java.util.UUID;
+
 /**
  * Created by oliverbuckley-salmon on 16/10/2016.
  */
 public class VenueLoader {
+    static final String[] executionVenues = {"FXALL", "LMAX", "", "Currenex", "Bloomberg", "ICAP", "Tullet", "360T", "EBS", "CME"};
+
+    public static void main(String[] argv) {
+        HazelcastInstance hazelcastInstance = Hazelcast.newHazelcastInstance();
+        Map<String, Venue> venuesCache = hazelcastInstance.getReplicatedMap("venue");
+        for (int i = 0; i < executionVenues.length; i++) {
+            Venue venue = new Venue(UUID.randomUUID().toString(), executionVenues[i], VenueTypeEnum.valueOf("ELECTRONIC"));
+            venuesCache.put(venue.getVenueId(), venue);
+        }
+
+    }
 }
